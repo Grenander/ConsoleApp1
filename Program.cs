e2262ef8b61addba1b463e677d3fe6789297f41d
Grenander <johny@grenanderaudio.com>
2023-03-27 10:54:56 +0200

    Now using a capsulated SQL class to simplify

--- Program.cs
+++ Program.cs
@@ -13,95 +13,95 @@ namespace ConsoleApp1
     using System.Data.Common;
     using System.Runtime.CompilerServices;
 
-
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("mySQL Test\n");
-
             testMySQL();
 
             Console.WriteLine("\nPress <enter> to quit...");
             while (Console.ReadKey().Key != ConsoleKey.Enter) { }
         }
 
-        private static void DisplayData(System.Data.DataTable table)
-        {
-            foreach (System.Data.DataRow row in table.Rows)
-            {
-                //Console.WriteLine("<" + row.ToString() + ">");
-                foreach (System.Data.DataColumn col in table.Columns)
-                {
-                    Console.WriteLine("{0} = {1}", col.ColumnName, row[col]);
-                    //Console.WriteLine("{0} = {1}", col.ColumnName, row[col]);
-                }
-                Console.WriteLine("============================");
-            }
-            Console.WriteLine("\n");
-        }
-
-        private static int ExecuteQuery(MySqlConnection conn, string query)
-        {
-            MySqlConnector.MySqlCommand myCommand = new MySqlConnector.MySqlCommand(query);
-            myCommand.Connection = conn;
-            conn.Open();
-            int n = myCommand.ExecuteNonQuery();
-            myCommand.Connection.Close();
-
-            return n;
-        }
-
         static void testMySQL()
         {
-            MySqlConnection conn;
+            SqlManager sql = new SqlManager();
 
-            string myConnectionString = "server=gsw.dnsalias.net;uid=brandtransparency;" +
-                                        "pwd=db23/M3rm2EA;database=brandtransparency";
+            if (! sql.Connect("server=gsw.dnsalias.net;uid=brandtransparency;" +
+                              "pwd=db23/M3rm2EA;database=brandtransparency"))
+            {
+                Console.WriteLine("Failed to connect to the database!");
+                return;
+            }
+            Console.WriteLine("Connect to the database was successful.\n");
+
+            string tableName = "jg_table";           // <- change this to whatever you like...
+
+            // Create table
+            sql.ExecuteQuery("CREATE TABLE IF NOT EXISTS `brandtransparency`.`" + tableName + "` " +
+                             "(`index` INT NOT NULL,`name` VARCHAR(64) NULL, PRIMARY KEY (`index`)) ENGINE = InnoDB");
+
+            // Delete all rows in the table
+            sql.ExecuteQuery("DELETE FROM " + tableName);
+
+            // Insert rows
+            sql.ExecuteQuery("INSERT INTO " + tableName + " VALUES (1, 'Pelle' )");
+            sql.ExecuteQuery("INSERT INTO " + tableName + " VALUES (2, 'Bertil' )");
+            sql.ExecuteQuery("INSERT INTO " + tableName + " VALUES (3, 'Sven' )");
+            sql.ExecuteQuery("INSERT INTO " + tableName + " VALUES (4, 'Pelle' )");
+
+            // Rename a rows name with Pelle to Hugo...
+            sql.ExecuteQuery("UPDATE " + tableName + " SET name = 'Hugo' WHERE name = 'Pelle'");
+
+
+            // Read and list table contents...
+            Console.WriteLine("1: list all\n");
 
             try
             {
-                conn = new MySqlConnection(myConnectionString);
-                //conn.Open();    // <- is it necessary ??? it just take time...
-                // ...
-                Console.WriteLine("Connection to MySQL successful!\n");
-                // ...
+                MySqlCommand cmd = sql.GetSqlCommand("SELECT * FROM " + tableName);
 
-                //DataTable table = conn.GetSchema("MetaDataCollections");
-                //DataTable table = conn.GetSchema();
-                //DisplayData(table);
-
-                // Query test...
-
-                // Remove table
-                /*
-                ExecuteQuery(conn, "DROP TABLE test_table");
-
-                Console.WriteLine("\nPress <enter> to continue...");
-                while (Console.ReadKey().Key != ConsoleKey.Enter) { }
-                */
-
-                // Create table
-                ExecuteQuery(conn, "CREATE TABLE IF NOT EXISTS `brandtransparency`.`test_table` " +
-                                   "(`index` INT NOT NULL,`name` VARCHAR(64) NULL, PRIMARY KEY (`index`)) ENGINE = InnoDB");
-
-                // Insert rows
-                ExecuteQuery(conn, "INSERT INTO test_table VALUES (1, 'Pelle' )");
-                ExecuteQuery(conn, "INSERT INTO test_table VALUES (2, 'Bertil' )");
-
-                // Rename row name
-                ExecuteQuery(conn, "UPDATE test_table SET name = 'Hugo' WHERE name = 'Pelle'");
-
-                Console.WriteLine("\nDone...");
-
-                conn.Close();
+                sql.Open();
+    
+                using (MySqlDataReader reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        Console.WriteLine("{0} {1}", reader.GetInt32(0), reader.GetString(1));
+                    }
+                }
             }
             catch (Exception ex)
             {
                 Console.WriteLine(ex.Message + "\n");
                 //MessageBox.Show(ex.Message);
-            }            
-        }        
-    }
+            }
 
+
+            Console.WriteLine("\n2: filtered list on the name 'Hugo'\n");
+
+            try
+            {
+                MySqlCommand cmd = sql.GetSqlCommand("SELECT * FROM " + tableName + " WHERE name = 'Hugo'");
+
+                //sql.Open();   // <- not necessary since we open above...
+
+                using (MySqlDataReader reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        Console.WriteLine("{0} {1}", reader.GetInt32(0), reader.GetString(1));
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message + "\n");
+                //MessageBox.Show(ex.Message);
+            }
+
+            Console.WriteLine("\nTest done...");
+        }
+
+    }
 }
